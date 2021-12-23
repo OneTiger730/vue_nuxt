8a91340af4a45fea7bbbcf91884975438a07a2f0
OneTiger730 <92943260+OneTiger730@users.noreply.github.com>
2021-12-22 19:39:08 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,9 +28,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '~/plugins/version.js'
-  ],
+  plugins: ['~/plugins/version.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
